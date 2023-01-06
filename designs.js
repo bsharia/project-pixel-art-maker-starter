34cdac6d33989bb34f014433e64f5eb2e3252bd6
Benjamin SA <benjamynfrank101@gmail.com>
2023-01-06 11:10:34 +0100

    draw the grid

--- designs.js
+++ designs.js
@@ -1,10 +1,42 @@
-// Select color input
-// Select size input
+
+// Select the table
+const table = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+function makeGrid(event) {
 
-function makeGrid() {
+    event.preventDefault();
 
-// Your code goes here!
+    // Your code goes here!
 
+    // Select the input sizes
+    const sizeH = document.querySelector("#inputHeight").value;
+    const sizeW = document.querySelector("#inputWidth").value;
+    console.log(`drawing a table of ${sizeH} by ${sizeW}`);
+
+    // Select the table
+
+    // Draw the table with provide h x w
+    // height = number of rows
+    // width = number of columns
+    createTable(sizeH, sizeW);
+
+}
+
+// Draw the table of m-height by n-width
+function createTable(m, n){
+    table.innerHTML = ""
+    const tbody = document.createElement("tbody");
+
+    for (let i = 0; i < m; i++) {
+        let row = document.createElement("tr");
+        for (let j = 0; j < n; j++) {
+            let cell = document.createElement("td");
+            // cell.textContent = `Row ${i+1}, Column ${j+1}`;
+            cell.textContent = `  `;
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody)
 }
