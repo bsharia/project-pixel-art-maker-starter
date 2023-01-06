fc937d3c243a167db5ecc8c7e8f1893f7d1767f2
Benjamin SA <benjamynfrank101@gmail.com>
2023-01-06 11:25:20 +0100

    update code to adhere to style guide

--- designs.js
+++ designs.js
@@ -3,27 +3,31 @@
 const table = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+/**
+ * @description Responds to a html form submission
+ * @param {string} event - the submit form event
+ */
 function makeGrid(event) {
 
     event.preventDefault();
 
-    // Your code goes here!
-
     // Select the input sizes
     const sizeH = document.querySelector("#inputHeight").value;
     const sizeW = document.querySelector("#inputWidth").value;
-    console.log(`drawing a table of ${sizeH} by ${sizeW}`);
+    // console.log(`drawing a table of ${sizeH} by ${sizeW}`);
 
-    // Select the table
-
-    // Draw the table with provide h x w
+    // Draw the table with provided h x w
     // height = number of rows
     // width = number of columns
     createTable(sizeH, sizeW);
 
 }
 
-// Draw the table of m-height by n-width
+/**
+ * @description Draw the table of m-height by n-width
+ * @param {integer} m - the height of the table 
+ * @param {integer} n - width of the table
+ */
 function createTable(m, n){
     table.innerHTML = ""
     const tbody = document.createElement("tbody");
@@ -41,9 +45,14 @@ function createTable(m, n){
     table.appendChild(tbody)
 }
 
-// Add on click listener to the table to paint cells
+// Add on click listener to the table to paint cell
 table.addEventListener("click", paintCanvas);
 
+
+/**
+ * @description Respond to a click event on a cell of the table
+ * @param {string} event - the click event passed to the function
+ */
 function paintCanvas(event) {
     // Select color input
     const color = document.querySelector("#colorPicker").value;
